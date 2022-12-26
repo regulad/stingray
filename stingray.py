a13a7b01f3977ea7ac2524d5bf58ca59a5d7c8ec
regulad <regulad@regulad.xyz>
2022-12-26 12:42:16 -0500

    update with logger

--- stingray.py
+++ stingray.py
@@ -7,12 +7,17 @@ from os import environ, remove
 from sys import stdin, stderr, stdout
 from os.path import exists, join
 from subprocess import run, Popen, DEVNULL
+from logging import getLogger, basicConfig, INFO
+
+logger = getLogger(__name__)
 
 parser = ArgumentParser(description="Stingray Server Script")
 parser.add_argument("--distro", dest="distro", type=str)
 parser.add_argument("--desktop", dest="desktop", type=str)
 
 args = parser.parse_args()
+
+basicConfig(level=INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 print("                                                                                ")
 print("                    .;,                                                         ")
@@ -56,8 +61,8 @@ print(" _\ \ \   ( ( (       / / / \ \ \   / / \ \ \_/\__\ \ \ \ \ \   / / _ \ \
 print(")____) )   \ \ \     ( (_(   )_) \ (_(   )_)  \/ _/  )_) ) \ \ ( (_( )_) )   / / /    ")
 print("\____\/    /_/_/      \/_/   \_\/ \/_/   \_\____/    \_\/ \_\/  \/_/ \_\/    \/_/     ")
 
-print("Starting Stingray server...")
-print(args.desktop + " on " + args.distro)
+logger.info("Starting Stingray server...")
+logger.info(str(args.desktop) + " on " + str(args.distro))
 
 print()
 
@@ -67,13 +72,15 @@ if exists(join(environ["PATH"], ".vnc", "passwd")):
     remove(join(environ["PATH"], ".vnc", "passwd"))
 # Delete the password file if it exists.
 
+# Create a new password
 run(["vncpasswd"], input=bytes(f"{environ['VNC_PASSWORD']}\n{environ['VNC_PASSWORD']}\n\n", encoding="UTF-8"))
 
-print()
+logger.info("Password configured")
 
+# Start the VNC server
 run(["vncserver", ":1", "-geometry", f"{environ['VNC_WIDTH']}x{environ['VNC_HEIGHT']}", "-depth", "24", "-name", f"Stringray-{environ['P_SERVER_UUID']}"])
 
-print()
+logger.info("VNC server started.")
 
 shell_env = {}
 
@@ -89,26 +96,19 @@ if shell_path is None:
         shell_path = "/bin/ash"
     elif exists(join("bin", "sh")):
         shell_path = "/bin/sh"
-        
-if shell_path is not None:
-    print("Will attempt to open shell " + shell_path)
 
 try:
     with Popen([f"/opt/websockify/websockify-{environ['WEBSOCKIFY_VERSION']}/run", environ["SERVER_PORT"], "localhost:5901"], stdin=DEVNULL) as websockify:
-        print()
-        print("Ready for connection!")
+        logger.info("Ready for connection!")
         if shell_path is not None:
+            logger.info("Will attempt to open shell " + shell_path)
             with Popen([shell_path], stdin=stdin, stderr=stderr, stdout=stdout) as shell:
-                print()
-                print("Shell opened!")
-                print()
+                logger.info("Shell opened!")
                 return_code = shell.wait()
-                print()
-                print("Shell process returned exit code " + return_code)
+                logger.info("Shell process returned exit code " + return_code)
         else:
             websockify.wait()
 except KeyboardInterrupt:
     websockify.terminate()
-    print()
-    print("Websockify process returned exit code " + websockify.returncode)
+    logger.info("Websockify process returned exit code " + websockify.returncode)
     run(["vncserver", "-clean", "-kill", ":1"], stdin=DEVNULL)
